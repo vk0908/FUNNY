705891aaa9b4b3dddacd2d79df1fffa0e8b3c927
demonlord2002 <drdoom2003p@gmail.com>
2025-10-06 00:12:27 +0530

    Update dir.py

--- AviaxMusic/core/dir.py
+++ AviaxMusic/core/dir.py
@@ -1,20 +1,18 @@
 import os
-
 from ..logging import LOGGER
 
+BASE_DIR = os.getcwd()
+DOWNLOAD_DIR = os.path.join(BASE_DIR, "downloads")
+COUPLE_DIR = os.path.join(BASE_DIR, "couples")
+CACHE_DIR = os.path.join(BASE_DIR, "cache")
 
 def dirr():
     for file in os.listdir():
-        if file.endswith(".jpg"):
-            os.remove(file)
-        elif file.endswith(".jpeg"):
-            os.remove(file)
-        elif file.endswith(".png"):
+        if file.lower().endswith((".jpg", ".jpeg", ".png")):
             os.remove(file)
 
-    if "downloads" not in os.listdir():
-        os.mkdir("downloads")
-    if "cache" not in os.listdir():
-        os.mkdir("cache")
+    os.makedirs(DOWNLOAD_DIR, exist_ok=True)
+    os.makedirs(CACHE_DIR, exist_ok=True)
+    os.makedirs(COUPLE_DIR, exist_ok=True)
 
     LOGGER(__name__).info("Directories Updated.")
