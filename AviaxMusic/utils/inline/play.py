8462f84904183c5235a447c3b008b918af9d64d4
cookies2002 <cookies2002boy@gmail.com>
2025-10-19 20:05:03 +0530

    Update play.py

--- AviaxMusic/utils/inline/play.py
+++ AviaxMusic/utils/inline/play.py
@@ -1,9 +1,6 @@
 import math
-
 from pyrogram.types import InlineKeyboardButton
-
 from AviaxMusic.utils.formatters import time_to_seconds
-
 
 def track_markup(_, videoid, user_id, channel, fplay):
     buttons = [
@@ -16,13 +13,7 @@ def track_markup(_, videoid, user_id, channel, fplay):
                 text=_["P_B_2"],
                 callback_data=f"MusicStream {videoid}|{user_id}|v|{channel}|{fplay}",
             ),
-        ],
-        [
-            InlineKeyboardButton(
-                text=_["CLOSE_BUTTON"],
-                callback_data=f"forceclose {videoid}|{user_id}",
-            )
-        ],
+        ]
     ]
     return buttons
 
@@ -33,25 +24,25 @@ def stream_markup_timer(_, chat_id, played, dur):
     percentage = (played_sec / duration_sec) * 100
     umm = math.floor(percentage)
     if 0 < umm <= 10:
-        bar = "◉—————————"
+        bar = "⚪—————————"
     elif 10 < umm < 20:
-        bar = "—◉————————"
+        bar = "—⚪————————"
     elif 20 <= umm < 30:
-        bar = "——◉———————"
+        bar = "——⚪———————"
     elif 30 <= umm < 40:
-        bar = "———◉——————"
+        bar = "———⚪——————"
     elif 40 <= umm < 50:
-        bar = "————◉—————"
+        bar = "————⚪—————"
     elif 50 <= umm < 60:
-        bar = "—————◉————"
+        bar = "—————⚪————"
     elif 60 <= umm < 70:
-        bar = "——————◉———"
+        bar = "——————⚪———"
     elif 70 <= umm < 80:
-        bar = "———————◉——"
+        bar = "———————⚪——"
     elif 80 <= umm < 95:
-        bar = "————————◉—"
+        bar = "————————⚪—"
     else:
-        bar = "—————————◉"
+        bar = "—————————✨"
     buttons = [
         [
             InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
@@ -66,7 +57,6 @@ def stream_markup_timer(_, chat_id, played, dur):
                 callback_data="GetTimer",
             )
         ],
-        [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
     return buttons
 
@@ -79,8 +69,7 @@ def stream_markup(_, chat_id):
             InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
-        ],
-        [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
+        ]
     ]
     return buttons
 
@@ -96,13 +85,7 @@ def playlist_markup(_, videoid, user_id, ptype, channel, fplay):
                 text=_["P_B_2"],
                 callback_data=f"AviaxPlaylists {videoid}|{user_id}|{ptype}|v|{channel}|{fplay}",
             ),
-        ],
-        [
-            InlineKeyboardButton(
-                text=_["CLOSE_BUTTON"],
-                callback_data=f"forceclose {videoid}|{user_id}",
-            ),
-        ],
+        ]
     ]
     return buttons
 
@@ -114,13 +97,7 @@ def livestream_markup(_, videoid, user_id, mode, channel, fplay):
                 text=_["P_B_3"],
                 callback_data=f"LiveStream {videoid}|{user_id}|{mode}|{channel}|{fplay}",
             ),
-        ],
-        [
-            InlineKeyboardButton(
-                text=_["CLOSE_BUTTON"],
-                callback_data=f"forceclose {videoid}|{user_id}",
-            ),
-        ],
+        ]
     ]
     return buttons
 
@@ -142,10 +119,6 @@ def slider_markup(_, videoid, user_id, query, query_type, channel, fplay):
             InlineKeyboardButton(
                 text="◁",
                 callback_data=f"slider B|{query_type}|{query}|{user_id}|{channel}|{fplay}",
-            ),
-            InlineKeyboardButton(
-                text=_["CLOSE_BUTTON"],
-                callback_data=f"forceclose {query}|{user_id}",
             ),
             InlineKeyboardButton(
                 text="▷",
