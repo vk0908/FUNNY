a8dab049b4d09d53df3cd7b8daf80a9163c865e3
vk0908 <rubesh08virat@gmail.com>
2025-10-24 16:03:39 +0530

    Update thumbnails.py

--- AviaxMusic/utils/thumbnails.py
+++ AviaxMusic/utils/thumbnails.py
@@ -45,7 +45,7 @@ def get_overlay_content_box(overlay_img: Image.Image) -> tuple:
     return binary.getbbox()
 
 
-async def get_thumb(videoid: str):
+async def  gen_thumb(videoid: str):
     url = f"https://www.youtube.com/watch?v={videoid}"
     try:
         results = VideosSearch(url, limit=1)
@@ -165,7 +165,8 @@ async def get_thumb(videoid: str):
         return tpath
 
     except Exception as e:
-        print(f"[get_thumb Error] {e}")
+        print(f"[gen_thumb Error] {e}")
         traceback.print_exc()
         return None 
+
 
